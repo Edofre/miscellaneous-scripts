09bf5b197c709e343e7674ac07da1e7bfe02fb9f
Edo Freriks <edofre@gmail.com>
2018-04-25 23:17:30 +0200

    Copy certificates to folder

--- certificates.php
+++ certificates.php
@@ -1,12 +1,20 @@
 <?php
 
+// Make a certificates dir
+if (!is_dir('certificates')) {
+    mkdir('certificates');
+}
 
-
+// Find all our .crt files
 if ($handle = opendir('/etc/nginx/ssl')) {
     while (false !== ($file = readdir($handle))) {
         if ($file != "." && $file != ".." && strtolower(substr($file, strrpos($file, '.') + 1)) == 'crt') {
-            var_dump($file);
+
+            copy("/etc/nginx/ssl/{$file}", "certificates/{$file}");
+
+            var_dump("Copied: {$file}");
         }
     }
     closedir($handle);
 }
+
